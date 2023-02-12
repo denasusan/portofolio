1796018b80dde11a75ba772e4686555daa9c483e
denasusan <56953905+denasusan@users.noreply.github.com>
2023-02-12 13:58:14 +0700

    initial to 2

--- pages/index.js
+++ pages/index.js
@@ -40,125 +40,139 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const swiperRef = useRef();
   const swiperRefOurCulture = useRef();
+  const [loading, setLoading] = useState(true);
   const [images, setImages] = useState([
     "/images/2.png",
     "/images/1.png"
   ]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 200)
+  }, [])
 
   return (
     <Layout>
-      <Header tabActive={1} />
-      <main>
-        <div>
-          <Swiper
-            slidesPerView={1}
-            ref={swiperRef}
-            breakpoints={SliderComponent}
-            // pagination={true}
-            modules={[Autoplay,Pagination]}
-            autoplay={{ delay: 10 }}
-            speed={1300}
-            onBeforeInit={(swiper) => {
-              swiperRef.current = swiper;
-            }}
-            loop={true}>
-            <div className='w-full text-blue-primary-light flex absolute z-10 lg:bottom-16 bottom-10 justify-between lg:px-12 opacity-50 px-2'>
-              <button className='transform rotate-180 text-blue-primary' onClick={() => swiperRef.current?.slidePrev()}><Redo fontSize='' /></button>
-              <button className='' onClick={() => swiperRef.current?.slideNext()}><Redo fontSize='' /></button>
-            </div>
-            {
-              images.map((item, index) => {
-                const image = item
-                return (
-                  <SwiperSlide key={index}>
-                    {
-                      <div className={`pb-10 w-full`}>
-                        <div className={`relative w-full`}>
+      {
+        loading == true ?
+          <div className='w-full min-h-screen flex justify-center items-center'>
+            <p>Loading...</p>
+          </div> :
+          <div>
+            <Header tabActive={1} />
+            <main>
+              <div>
+                <Swiper
+                  slidesPerView={1}
+                  ref={swiperRef}
+                  breakpoints={SliderComponent}
+                  // pagination={true}
+                  modules={[Autoplay, Pagination]}
+                  autoplay={{ delay: 10 }}
+                  speed={1300}
+                  onBeforeInit={(swiper) => {
+                    swiperRef.current = swiper;
+                  }}
+                  loop={true}>
+                  <div className='w-full text-blue-primary-light flex absolute z-10 lg:bottom-16 bottom-10 justify-between lg:px-12 opacity-50 px-2'>
+                    <button className='transform rotate-180 text-blue-primary' onClick={() => swiperRef.current?.slidePrev()}><Redo fontSize='' /></button>
+                    <button className='' onClick={() => swiperRef.current?.slideNext()}><Redo fontSize='' /></button>
+                  </div>
+                  {
+                    images.map((item, index) => {
+                      const image = item
+                      return (
+                        <SwiperSlide key={index}>
+                          {
+                            <div className={`pb-10 w-full`}>
+                              <div className={`relative w-full`}>
 
-                          <div className='' style={{ width: '100%', height: '100%', position: 'relative', paddingBottom: "51%" }}>
-                            <Image
-                              tabIndex="0"
-                              src={`${image}`}
-                              alt={`slide${index}`}
-                              layout='fill'
-                              placeholder='blur'
-                              blurDataURL='/'
-                              loading='lazy'
-                              objectFit='cover'
-                            />
-                          </div>
+                                <div className='' style={{ width: '100%', height: '100%', position: 'relative', paddingBottom: "51%" }}>
+                                  <Image
+                                    tabIndex="0"
+                                    src={`${image}`}
+                                    alt={`slide${index}`}
+                                    layout='fill'
+                                    placeholder='blur'
+                                    blurDataURL='/'
+                                    loading='lazy'
+                                    objectFit='cover'
+                                  />
+                                </div>
 
-                        </div>
-                      </div>
-                    }
-                  </SwiperSlide>
-                );
-              })
-            }
-          </Swiper>
-
-          <div className='w-9/12 mx-auto lg:my-10 my-0'>
-            <p className='lg:text-24px text-20px font-bold py-2 text-blue-primary '>About Us</p>
-            <p className='bg-blue-light rounded-xl p-4 text-14px lg:text-16px font-medium'>Gaya Properti didirikan pada tahun 2013 yang bermula sebagai pengembang perumahan khususnya di wilayah Kota Bandung, Jawa Barat. Seiring berkembangnya waktu, Gaya Properti bertransformasi menjadi perusahaan yang bergerak di bidang Kontraktor, Arsitektur, Desain Interior dan Custom Furniture.
-              <br/> <br/>
-              Gaya Properti bertujuan menjadi perusahaan konglomerasi properti yang dapat besaing di kancah Nasional Maupun Internasional dengan memberikan pelayanan yang terbaik untuk mitra dan klien, membangun sumber daya manusia yang berintegritas dan berkompetensi internasional serta membangun manajemen yang transparan dan profesional.
-            </p>
-          </div>
-
-          <div className='w-9/12 mx-auto mt-10'>
-            <p className='lg:text-24px text-20px font-bold py-2 text-blue-primary'>Our Culture</p>
-            <Swiper
-              slidesPerView={1}
-              ref={swiperRefOurCulture}
-              // breakpoints={SliderComponent}
-              // pagination={true}
-              modules={[Autoplay]}
-              autoplay={{ delay: 1000 , disableOnInteraction: false}}
-              speed={1000}
-              onBeforeInit={(swiper) => {
-                swiperRefOurCulture.current = swiper;
-              }}
-              loop={true}>
-              <div className='w-full text-blue-primary-light flex absolute z-10 lg:bottom-16 bottom-10 justify-between lg:px-12 opacity-50 px-2'>
-                <button className='transform rotate-180 text-blue-primary' onClick={() => swiperRefOurCulture.current?.slidePrev()}><Redo fontSize='' /></button>
-                <button className='' onClick={() => swiperRefOurCulture.current?.slideNext()}><Redo fontSize='' /></button>
-              </div>
-              {
-                images.map((item, index) => {
-                  const image = item
-                  return (
-                    <SwiperSlide key={index}>
-                      {
-                        <div className={`pb-10 w-full`}>
-                          <div className={`relative w-full`}>
-
-                            <div style={{ width: '100%', height: '100%', position: 'relative', paddingBottom: "51%" }}>
-                              <Image
-                                tabIndex="0"
-                                src={`${image}`}
-                                alt={`slide${index}`}
-                                layout='fill'
-                                placeholder='blur'
-                                blurDataURL='/'
-                                loading='lazy'
-                                objectFit='cover'
-                                className='rounded-xl'
-                              />
+                              </div>
                             </div>
+                          }
+                        </SwiperSlide>
+                      );
+                    })
+                  }
+                </Swiper>
 
-                          </div>
-                        </div>
-                      }
-                    </SwiperSlide>
-                  );
-                })
-              }
-            </Swiper>
+                <div className='w-9/12 mx-auto lg:my-10 my-0'>
+                  <p className='lg:text-24px text-20px font-bold py-2 text-blue-primary '>About Us</p>
+                  <p className='bg-blue-light rounded-xl p-4 text-14px lg:text-16px font-medium'>Gaya Properti didirikan pada tahun 2013 yang bermula sebagai pengembang perumahan khususnya di wilayah Kota Bandung, Jawa Barat. Seiring berkembangnya waktu, Gaya Properti bertransformasi menjadi perusahaan yang bergerak di bidang Kontraktor, Arsitektur, Desain Interior dan Custom Furniture.
+                    <br /> <br />
+                    Gaya Properti bertujuan menjadi perusahaan konglomerasi properti yang dapat besaing di kancah Nasional Maupun Internasional dengan memberikan pelayanan yang terbaik untuk mitra dan klien, membangun sumber daya manusia yang berintegritas dan berkompetensi internasional serta membangun manajemen yang transparan dan profesional.
+                  </p>
+                </div>
+
+                <div className='w-9/12 mx-auto mt-10'>
+                  <p className='lg:text-24px text-20px font-bold py-2 text-blue-primary'>Our Culture</p>
+                  <Swiper
+                    slidesPerView={1}
+                    ref={swiperRefOurCulture}
+                    // breakpoints={SliderComponent}
+                    // pagination={true}
+                    modules={[Autoplay]}
+                    autoplay={{ delay: 1000, disableOnInteraction: false }}
+                    speed={1000}
+                    onBeforeInit={(swiper) => {
+                      swiperRefOurCulture.current = swiper;
+                    }}
+                    loop={true}>
+                    <div className='w-full text-blue-primary-light flex absolute z-10 lg:bottom-16 bottom-10 justify-between lg:px-12 opacity-50 px-2'>
+                      <button className='transform rotate-180 text-blue-primary' onClick={() => swiperRefOurCulture.current?.slidePrev()}><Redo fontSize='' /></button>
+                      <button className='' onClick={() => swiperRefOurCulture.current?.slideNext()}><Redo fontSize='' /></button>
+                    </div>
+                    {
+                      images.map((item, index) => {
+                        const image = item
+                        return (
+                          <SwiperSlide key={index}>
+                            {
+                              <div className={`pb-10 w-full`}>
+                                <div className={`relative w-full`}>
+
+                                  <div style={{ width: '100%', height: '100%', position: 'relative', paddingBottom: "51%" }}>
+                                    <Image
+                                      tabIndex="0"
+                                      src={`${image}`}
+                                      alt={`slide${index}`}
+                                      layout='fill'
+                                      placeholder='blur'
+                                      blurDataURL='/'
+                                      loading='lazy'
+                                      objectFit='cover'
+                                      className='rounded-xl'
+                                    />
+                                  </div>
+
+                                </div>
+                              </div>
+                            }
+                          </SwiperSlide>
+                        );
+                      })
+                    }
+                  </Swiper>
+                </div>
+              </div>
+            </main>
+            <Footer />
           </div>
-        </div>
-      </main>
-      <Footer />
+      }
     </Layout>
   )
 }
