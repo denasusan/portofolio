b691831f433aa78df5e3ce924fb1e6d72d277f7d
denasusan <56953905+denasusan@users.noreply.github.com>
2023-03-01 09:00:24 +0700

    gaya-properti v0.1.3

--- pages/portofolio.js
+++ pages/portofolio.js
@@ -6,18 +6,18 @@ import { useEffect, useState } from "react";
 export default function Portofolio() {
     const [loading, setLoading] = useState(true);
     const [images, setImages] = useState([
-        "/images/pt-1.png",
-        "/images/pt-2.png",
-        "/images/pt-3.png",
-        "/images/pt-4.png",
-        "/images/pt-5.png",
-        "/images/pt-6.png",
-        "/images/pt-7.png",
-        "/images/pt-8.png",
-        "/images/pt-9.png",
-        "/images/pt-10.png",
-        "/images/pt-11.png",
-        "/images/pt-12.png",
+        "images/pt-1.png",
+        "images/pt-2.png",
+        "images/pt-3.png",
+        "images/pt-4.png",
+        "images/pt-5.png",
+        "images/pt-6.png",
+        "images/pt-7.png",
+        "images/pt-8.png",
+        "images/pt-9.png",
+        "images/pt-10.png",
+        "images/pt-11.png",
+        "images/pt-12.png",
     ]);
 
     useEffect(() => {
@@ -31,7 +31,7 @@ export default function Portofolio() {
             {
                 loading == true ?
                     <div className='w-full min-h-screen flex justify-center items-center animate-bounce'>
-                        <img src="/images/logo-small.png" />
+                        <img src="images/logo-small.png" />
                     </div> :
                     <div>
                         <Header tabActive={"portofolio"} />
