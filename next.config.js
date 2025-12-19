fab42d31b1d585950710bfe1ce4a030bb0ed2e29
Dena Susan <dena@sharinghappiness.org>
2025-12-19 14:00:55 +0700

    upgrade 2025

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
 
-module.exports = nextConfig
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
+  // GANTI dengan nama repo kamu
+  basePath: process.env.NODE_ENV === 'production' ? '/NAMA_REPO' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/NAMA_REPO/' : '',
+};
+
+module.exports = nextConfig;
